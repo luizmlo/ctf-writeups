723c0d6c2cf517ab2ca3466ab6d2f54fe0e8ca0e
Mlo <luizf.mlobrc@gmail.com>
2019-06-25 12:20:07 -0300

    Update terminator.asm

--- cpbsb3_ctf/terminator/terminator.asm
+++ cpbsb3_ctf/terminator/terminator.asm
@@ -12,14 +12,3 @@ loop:
   bne loop
   sta $13
   brk
-
-
-ff010000011326394c5f728598abbed1e4f70a1d
-
-
-ff 01 00 00 01 13 26 39 4c 5f 72 85 98 ab be d1 
-0010: e4 f7 0a 1d
-
-
-
-a8ec7f28be192280afd98525f879a960ab875247590bb491dffc283e00386b5f
